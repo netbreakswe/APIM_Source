d60926e4279ed6cab0db4d873a57cd8bb4b69664
Skiti <kutinju@libero.it>
2017-06-16 17:47:29 +0200

    Paypal Funzionante
    -Pulsante Paypal funzionante in ricarica crediti

--- Frontend/app/controllers/ricarica_crediti/ricarica_crediti.js
+++ Frontend/app/controllers/ricarica_crediti/ricarica_crediti.js
@@ -19,11 +19,262 @@ angular.module('APIM.ricarica_crediti')
 		}
 	});
 	
-	$scope.updateCredits = function() {
-		$http.post("http://localhost:8101/credits_update?IdClient=" + localStorage.getItem("IdClient") + "&Credits=" + 100).then(function(response) {
-			console.log("Ciao");
-			window.location.reload();
-		});
-	};
+	// pulsante 100
+	paypal.Button.render({
+
+		env: 'sandbox', // sandbox | production
+
+		// PayPal Client IDs - replace with your own
+		// Create a PayPal app: https://developer.paypal.com/developer/applications/create
+		client: {
+			sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+			production: '<insert production client id>'
+		},
+
+		// Show the buyer a 'Pay Now' button in the checkout flow
+		commit: true,
+
+		// payment() is called when the button is clicked
+		payment: function(data, actions) {
+
+			// Make a call to the REST api to create the payment
+			return actions.payment.create({
+				transactions: [{
+					amount: { total: '1', currency: 'EUR' }
+				}]
+			});
+		},
+
+		// onAuthorize() is called when the buyer approves the payment
+		onAuthorize: function(data, actions) {
+			
+			function updateCredits() {
+				$http.post("http://localhost:8101/credits_update?IdClient=" + localStorage.getItem("IdClient") + "&Credits=" + 100).then(function(response) {
+					window.location.reload();
+				});
+			};
+
+			// Make a call to the REST api to execute the payment
+			return actions.payment.execute().then(function(param) {
+				updateCredits();
+			});
+		}
+
+	}, '#paypal-button-container100');
+	
+	// pulsante 200
+	paypal.Button.render({
+
+		env: 'sandbox', // sandbox | production
+
+		// PayPal Client IDs - replace with your own
+		// Create a PayPal app: https://developer.paypal.com/developer/applications/create
+		client: {
+			sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+			production: '<insert production client id>'
+		},
+
+		// Show the buyer a 'Pay Now' button in the checkout flow
+		commit: true,
+
+		// payment() is called when the button is clicked
+		payment: function(data, actions) {
+
+			// Make a call to the REST api to create the payment
+			return actions.payment.create({
+				transactions: [{
+					amount: { total: '2', currency: 'EUR' }
+				}]
+			});
+		},
+
+		// onAuthorize() is called when the buyer approves the payment
+		onAuthorize: function(data, actions) {
+			
+			function updateCredits() {
+				$http.post("http://localhost:8101/credits_update?IdClient=" + localStorage.getItem("IdClient") + "&Credits=" + 200).then(function(response) {
+					window.location.reload();
+				});
+			};
+
+			// Make a call to the REST api to execute the payment
+			return actions.payment.execute().then(function(param) {
+				updateCredits();
+			});
+		}
+
+	}, '#paypal-button-container200');
+	
+	// pulsante 500
+	paypal.Button.render({
+
+		env: 'sandbox', // sandbox | production
+
+		// PayPal Client IDs - replace with your own
+		// Create a PayPal app: https://developer.paypal.com/developer/applications/create
+		client: {
+			sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+			production: '<insert production client id>'
+		},
+
+		// Show the buyer a 'Pay Now' button in the checkout flow
+		commit: true,
+
+		// payment() is called when the button is clicked
+		payment: function(data, actions) {
+
+			// Make a call to the REST api to create the payment
+			return actions.payment.create({
+				transactions: [{
+					amount: { total: '5', currency: 'EUR' }
+				}]
+			});
+		},
+
+		// onAuthorize() is called when the buyer approves the payment
+		onAuthorize: function(data, actions) {
+			
+			function updateCredits() {
+				$http.post("http://localhost:8101/credits_update?IdClient=" + localStorage.getItem("IdClient") + "&Credits=" + 500).then(function(response) {
+					window.location.reload();
+				});
+			};
+
+			// Make a call to the REST api to execute the payment
+			return actions.payment.execute().then(function(param) {
+				updateCredits();
+			});
+		}
+
+	}, '#paypal-button-container500');
+	
+	// pulsante 1000
+	paypal.Button.render({
+
+		env: 'sandbox', // sandbox | production
+
+		// PayPal Client IDs - replace with your own
+		// Create a PayPal app: https://developer.paypal.com/developer/applications/create
+		client: {
+			sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+			production: '<insert production client id>'
+		},
+
+		// Show the buyer a 'Pay Now' button in the checkout flow
+		commit: true,
+
+		// payment() is called when the button is clicked
+		payment: function(data, actions) {
+
+			// Make a call to the REST api to create the payment
+			return actions.payment.create({
+				transactions: [{
+					amount: { total: '9', currency: 'EUR' }
+				}]
+			});
+		},
+
+		// onAuthorize() is called when the buyer approves the payment
+		onAuthorize: function(data, actions) {
+			
+			function updateCredits() {
+				$http.post("http://localhost:8101/credits_update?IdClient=" + localStorage.getItem("IdClient") + "&Credits=" + 1000).then(function(response) {
+					window.location.reload();
+				});
+			};
+
+			// Make a call to the REST api to execute the payment
+			return actions.payment.execute().then(function(param) {
+				updateCredits();
+			});
+		}
+
+	}, '#paypal-button-container1000');
+	
+	// pulsante 2000
+	paypal.Button.render({
+
+		env: 'sandbox', // sandbox | production
+
+		// PayPal Client IDs - replace with your own
+		// Create a PayPal app: https://developer.paypal.com/developer/applications/create
+		client: {
+			sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+			production: '<insert production client id>'
+		},
+
+		// Show the buyer a 'Pay Now' button in the checkout flow
+		commit: true,
+
+		// payment() is called when the button is clicked
+		payment: function(data, actions) {
+
+			// Make a call to the REST api to create the payment
+			return actions.payment.create({
+				transactions: [{
+					amount: { total: '17', currency: 'EUR' }
+				}]
+			});
+		},
+
+		// onAuthorize() is called when the buyer approves the payment
+		onAuthorize: function(data, actions) {
+			
+			function updateCredits() {
+				$http.post("http://localhost:8101/credits_update?IdClient=" + localStorage.getItem("IdClient") + "&Credits=" + 2000).then(function(response) {
+					window.location.reload();
+				});
+			};
+
+			// Make a call to the REST api to execute the payment
+			return actions.payment.execute().then(function(param) {
+				updateCredits();
+			});
+		}
+
+	}, '#paypal-button-container2000');
+	
+	// pulsante 5000
+	paypal.Button.render({
+
+		env: 'sandbox', // sandbox | production
+
+		// PayPal Client IDs - replace with your own
+		// Create a PayPal app: https://developer.paypal.com/developer/applications/create
+		client: {
+			sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+			production: '<insert production client id>'
+		},
+
+		// Show the buyer a 'Pay Now' button in the checkout flow
+		commit: true,
+
+		// payment() is called when the button is clicked
+		payment: function(data, actions) {
+
+			// Make a call to the REST api to create the payment
+			return actions.payment.create({
+				transactions: [{
+					amount: { total: '41', currency: 'EUR' }
+				}]
+			});
+		},
+
+		// onAuthorize() is called when the buyer approves the payment
+		onAuthorize: function(data, actions) {
+			
+			function updateCredits() {
+				$http.post("http://localhost:8101/credits_update?IdClient=" + localStorage.getItem("IdClient") + "&Credits=" + 5000).then(function(response) {
+					window.location.reload();
+				});
+			};
+
+			// Make a call to the REST api to execute the payment
+			return actions.payment.execute().then(function(param) {
+				updateCredits();
+			});
+		}
+
+	}, '#paypal-button-container5000');
 	
 });
